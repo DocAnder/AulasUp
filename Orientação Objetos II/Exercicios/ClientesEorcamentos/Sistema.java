f6def3c798be7f004bf80f883b8e49621d56211d
Doc <andersonmartinseoliveira@gmail.com>
2023-03-15 21:50:21 -0300

    att

---
+++ Orientação Objetos II/Exercicios/ClientesEorcamentos/Sistema.java
@@ -0,0 +1,18 @@
+public class Sistema {
+    
+    public static void main(String[] args) {
+        
+        int op = 1;
+
+        do {
+            
+
+            
+
+        } while (op != 0);
+
+
+    }
+
+
+}
