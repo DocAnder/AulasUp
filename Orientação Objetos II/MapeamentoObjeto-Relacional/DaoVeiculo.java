37f2930465cd99c94bd931acea66ef0db0054faf
DocAnder <106823729+DocAnder@users.noreply.github.com>
2023-04-12 22:00:16 -0300

    Add files via upload

--- Orientação Objetos II/MapeamentoObjeto-Relacional/DaoVeiculo.java
+++ Orientação Objetos II/MapeamentoObjeto-Relacional/DaoVeiculo.java
@@ -1,8 +1,9 @@
-import java.sql.Array;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DaoVeiculo {
     
@@ -47,6 +48,7 @@ public class DaoVeiculo {
         return resultado;
     }
 
+    //Forma de imprimir direto
     public void listarTodos(){ 
         ResultSet resultado;       
         try {
@@ -54,14 +56,99 @@ public class DaoVeiculo {
             String comando = "SELECT * from tb_veiculos;"; 
             resultado = st.executeQuery(comando);
             while (resultado.next()) {                
-                System.out.println(resultado.getString("MARCA"));
-                System.out.println(resultado.getString("MODELO"));
+                System.out.println(resultado.getString("MARCA")  + " " + resultado.getString("MODELO") + " " + resultado.getString("CHASSI") + " " + resultado.getString("ANO"));                
             }                     
         } catch (Exception e) {
             // TODO: handle exception
         }finally{
             this.desconectar();
         }        
+    }
+
+
+    public ArrayList<Veiculo> buscarTodos(){
+        ArrayList<Veiculo> resultados = new ArrayList<Veiculo>();
+
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos ORDER BY modelo;");
+            while (rs.next()) {
+                Veiculo aux = new Veiculo();
+                aux.setCodigo(rs.getInt("CODIGO"));
+                aux.setMarca(rs.getString("MARCA"));
+                aux.setModelo(rs.getString("MODELO"));
+                aux.setChassi(rs.getString("CHASSI"));
+                aux.setAno(rs.getInt("ANO"));
+                resultados.add(aux);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+
+        return resultados;
+    }
+
+    public Veiculo buscarUm(int codigo){
+        Veiculo aux = null;
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos WHERE codigo = " + codigo + ";");
+            if (rs.next()) {
+                aux = new Veiculo();
+                aux.setCodigo(rs.getInt("CODIGO"));
+                aux.setMarca(rs.getString("MARCA"));
+                aux.setModelo(rs.getString("MODELO"));
+                aux.setChassi(rs.getString("CHASSI"));
+                aux.setAno(rs.getInt("ANO"));    
+            }            
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return aux;
+    }
+
+    public int alterarUm(Veiculo veiculo){
+        int qtd = 0;
+        try {
+            this.conectar();
+            String comando = "UPDATE tb_veiculos SET " +
+            "marca = '" + veiculo.getMarca() + "'," +
+            "modelo = '" + veiculo.getModelo() + "'," +
+            "chassi = '" + veiculo.getChassi() + "'," +
+            "ano = " + veiculo.getAno() +
+            " WHERE codigo = " + veiculo.getCodigo() + ";";
+            System.out.println(comando);
+            st.executeUpdate(comando);
+            qtd = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir o registro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return qtd;
+    }
+
+
+
+
+    public int exlcluir(int codigo){
+        int resultado = 0;
+        try {
+            this.conectar();
+            String comando = "DELET FROM tb_veiculos WHERE codigo = " + codigo + ";";            
+            st.executeUpdate(comando);
+            //getUpdate recupera quantos dados foram apagados do banco            
+            resultado = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return resultado;
     }
 
 }
--- Orientação Objetos II/MapeamentoObjeto-Relacional/GerenciadorVeiculo.java
+++ Orientação Objetos II/MapeamentoObjeto-Relacional/GerenciadorVeiculo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GerenciadorVeiculo {
@@ -34,17 +35,19 @@ public class GerenciadorVeiculo {
                     this.cadastrar();
                     break;
                 case 2:
-                    //gerenciar clientes
+                    this.buscarUm();
                 case 3:
-                    System.out.println("Sistema encerrado!");                    
+                    this.alterar();                 
                     break;
                 case 4:
-
+                    this.excluir();
                     break;
                 case 5:
-
+                    this.listarTodos();                    
                     break;
                 case 0:
+                    Locadora aux = new Locadora();
+                    aux.menu();
                     break;
                 default:
                     System.out.println("Opção invalida!");
@@ -68,6 +71,95 @@ public class GerenciadorVeiculo {
         if (inserido) {
             System.out.println("Veiculo inserido com sucesso!");
         }
+
+    }
+
+    public void excluir(){
+        System.out.println("-----===============================-----");
+        System.out.println("Exclusão de veículo");
+        System.out.println("Codigo: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        int quantidade = daoVeiculo.exlcluir(codigo);
+        if (quantidade > 0) {
+            System.out.println("Registro excluido com sucesso!");
+        } else {
+            System.out.println("Registro não encontrado...");
+        }
+    }
+
+    public void buscarUm(){
+        System.out.println("-----===============================-----");
+        System.out.println("Buscar veículo");
+        System.out.println("Codigo: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        Veiculo retorno = daoVeiculo.buscarUm(codigo);
+        if (retorno != null) {
+            System.out.println("Codigo: " + retorno.getCodigo() 
+                        + " Marca: " + retorno.getMarca() +
+                        " Modelo: " + retorno.getModelo() + 
+                        " Chassi: " + retorno.getChassi() + 
+                        " Ano: " + retorno.getAno());
+        } else {
+            System.out.println("Veiculo não encontrado!");
+        }
+    }
+
+    public void listarTodos(){
+        ArrayList<Veiculo> veiculos = daoVeiculo.buscarTodos();                     
+        for (Veiculo veiculo : veiculos) {
+            System.out.println("Codigo: " + veiculo.getCodigo() 
+                + " Marca: " + veiculo.getMarca() +
+                " Modelo: " + veiculo.getModelo() + 
+                " Chassi: " + veiculo.getChassi() + 
+                " Ano: " + veiculo.getAno());
+            }
+    }
+
+    public void alterar(){
+        System.out.println("-----===============================-----");
+        System.out.println("Alterar veículo");
+        System.out.println("Codigo: ");
+        int codigo = Integer.parseInt(scanner.nextLine());
+        Veiculo veiculo = daoVeiculo.buscarUm(codigo);
+        if (veiculo != null) {
+            System.out.println("Dados do Veículos:");
+            System.out.println("[Codigo: " + veiculo.getCodigo() + "]");
+
+            System.out.println("[Marca: " + veiculo.getMarca() + "]");
+            String marca = scanner.nextLine();
+            if (!marca.isEmpty()) {
+                veiculo.setMarca(marca);
+            }
+
+            System.out.println("[Modelo: " + veiculo.getModelo() + "]");
+            String modelo = scanner.nextLine();
+            if (!modelo.isEmpty()) {
+                veiculo.setModelo(modelo);
+            }
+
+            System.out.println("[Chassi: " + veiculo.getChassi() + "]");
+            String chassi = scanner.nextLine();
+            if (!chassi.isEmpty()) {
+                veiculo.setChassi(chassi);
+            }
+
+            System.out.println("[Ano: " + veiculo.getAno() + "]");
+            String ano = scanner.nextLine();
+            if (!ano.isEmpty()) {
+                veiculo.setAno(Integer.parseInt(ano));
+            }       
+            
+            int alterados = daoVeiculo.alterarUm(veiculo);
+            if (alterados > 0) {
+                System.out.println("Veiculo alterado com sucesso!");
+            }
+                
+
+        }else{
+            System.out.println("Veiculo não encontrado!");
+        }
+
+        
 
     }
 
