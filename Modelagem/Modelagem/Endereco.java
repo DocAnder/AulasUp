842a9513a18e517e98273af54dcc3a948b839ac5
DocAnder <106823729+DocAnder@users.noreply.github.com>
2023-05-23 20:03:02 -0300

    Add files via upload

---
+++ Modelagem/Modelagem/Endereco.java
@@ -0,0 +1,23 @@
+public class Endereco {
+
+	private int id;
+
+	private String cep;
+
+	private String nomeLogradouro;
+
+	private String numero;
+
+	private String complemento;
+
+	private String bairro;
+
+	private String cidade;
+
+	private String estado;
+
+	private String pais;
+
+	private TipoLogradouro tipoLogradouro;
+
+}
